57c5653f6424c0853e49c334ba588ad9e4e390f5
tanimuranaomichi <tanimuranaomichi@gmail.com>
2021-11-07 01:39:35 +0900

    perf: アカウント登録画面へのリンク追加

--- src/screens/login.js
+++ src/screens/login.js
@@ -4,6 +4,9 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
@@ -30,7 +33,7 @@ const Login = () => {
                 sm={4}
                 md={7}
                 sx={{
-                    backgroundImage: 'url(https://source.unsplash.com/featured/?programming)',
+                    backgroundImage: 'url(https://source.unsplash.com/random)',
                     backgroundRepeat: 'no-repeat',
                     backgroundColor: (t) =>
                         t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
@@ -83,6 +86,11 @@ const Login = () => {
                         >
                             Sign In
                         </Button>
+                        <Box textAlign="right">
+                            <Link href="/signup" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Box>
                     </Box>
                 </Box>
             </Grid>
