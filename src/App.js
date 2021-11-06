620c26946338334e6216737e3a9c24876665375c
tanimuranaomichi <tanimuranaomichi@gmail.com>
2021-11-07 00:19:45 +0900

    perf: ドロワーの表示・画面遷移機能の追加

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+
 import Typography from '@mui/material/Typography';
-import { ThemeProvider } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
+import { ThemeProvider, Drawer } from '@mui/material';
+import { createTheme, styled } from '@mui/material/styles';
 import { blue, green } from '@mui/material/colors';
 import { GlobalStyles } from '@mui/material';
 import Home from './screens/home';
 import Login from './screens/login';
 import Questionnaire from './screens/questionnaire';
 import Archivement from './screens/achievement';
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
 
 const theme = createTheme({
   palette: {
@@ -26,6 +40,17 @@ const theme = createTheme({
 });
 
 const App = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+
   return (
     <>
       <GlobalStyles styles={{ body: { margin: 0, padding: 0 } }} />
@@ -33,11 +58,10 @@ const App = () => {
       <AppBar position="static">
         <Toolbar>
           <IconButton
-            size="large"
-            edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleDrawerOpen}
           >
             <MenuIcon />
           </IconButton>
@@ -46,6 +70,40 @@ const App = () => {
           </Typography>
         </Toolbar>
       </AppBar>
+      <Drawer
+        sx={{
+          width: 240,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: 240,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </DrawerHeader>
+
+        <List>
+          <ListItemButton component="a" href="/">
+            <ListItemText primary="ホーム" />
+          </ListItemButton>
+          <ListItemButton component="a" href="/login">
+            <ListItemText primary="ログイン" />
+          </ListItemButton>
+          <ListItemButton component="a" href="/questionnaire">
+            <ListItemText primary="アンケート" />
+          </ListItemButton>
+          <ListItemButton component="a" href="/achievement">
+            <ListItemText primary="実績" />
+          </ListItemButton>
+        </List>
+      </Drawer>
       <BrowserRouter>
         <Switch>
           <Route path='/login' component={Login} />
