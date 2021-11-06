2fd16507489fe5250354d19b1bc0d28d9867ca50
tanimuranaomichi <tanimuranaomichi@gmail.com>
2021-11-06 18:07:40 +0900

    feat: firebaseへの実績登録機能追加

--- src/screens/achievement.js
+++ src/screens/achievement.js
@@ -1,10 +1,14 @@
 // 実績入力画面
-import React from 'react';
+import React, { useState } from 'react';
 import './achievement.css'
 import { Container, Paper, Typography, TextField, Button } from '@mui/material';
 import { Box } from '@mui/system';
+import { pushAchievement } from "../firebase"
 
 const Achievement = () => {
+    const [eventName, setEventName] = useState()
+    const [result, setResult] = useState()
+
     return (
         <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
             <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
@@ -17,16 +21,25 @@ const Achievement = () => {
                         label="イベント名"
                         fullWidth
                         variant="standard"
+                        value={eventName}
+                        onChange={(e) => setEventName((eventName) => (eventName = e.target.value))}
                     />
                     <TextField
                         required
                         label="成績"
                         fullWidth
                         variant="standard"
+                        value={result}
+                        onChange={(e) => setResult((result) => (result = e.target.value))}
                     />
                 </Box>
                 <Box textAlign="right">
-                    <Button variant="contained">登録</Button>
+                    <Button
+                        variant="contained"
+                        onClick={() => pushAchievement({ eventName: eventName, result: result })}
+                    >
+                        登録
+                    </Button>
                 </Box>
             </Paper >
         </Container >
