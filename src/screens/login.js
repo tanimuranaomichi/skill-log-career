1507b13a484b9458972ae62db01b9f85cc3c988c
tanimuranaomichi <tanimuranaomichi@gmail.com>
2021-11-06 10:14:25 +0900

    refacter: クラスコンポーネントを関数コンポーネントに変更

--- src/screens/login.js
+++ src/screens/login.js
@@ -2,12 +2,10 @@
 import React from 'react';
 import './login.css'
 
-class Home extends React.Component {
-    render() {
-        return (
-            <h1>ログイン</h1> // 消してください by谷村
-        );
-    }
+const Home = () => {
+    return (
+        <h1>ログイン</h1> // 消してください by谷村
+    );
 }
 
 export default Home;
