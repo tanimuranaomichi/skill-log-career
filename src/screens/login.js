1c9c6c92ccec24d53cb7bf2b2cab508413210540
tanimuranaomichi <tanimuranaomichi@gmail.com>
2021-11-07 01:56:34 +0900

    fix: 背景画像URLの修正

--- src/screens/login.js
+++ src/screens/login.js
@@ -33,7 +33,7 @@ const Login = () => {
                 sm={4}
                 md={7}
                 sx={{
-                    backgroundImage: 'url(https://source.unsplash.com/random)',
+                    backgroundImage: 'url(https://source.unsplash.com/featured/?programming)',
                     backgroundRepeat: 'no-repeat',
                     backgroundColor: (t) =>
                         t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
