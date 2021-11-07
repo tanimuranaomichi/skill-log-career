099b08617624305c6b42bbd0e4f0c7d31a60507f
tanimuranaomichi <tanimuranaomichi@gmail.com>
2021-11-07 10:25:55 +0900

    refacter: console.logの消去

--- src/components/privateRoute.js
+++ src/components/privateRoute.js
@@ -2,12 +2,9 @@ import { Route, Redirect } from 'react-router-dom';
 import { useAuthContext } from '../context/authContext';
 const PrivateRoute = ({ component, exact, path }) => {
     const { user } = useAuthContext();
-    console.log(user);
     if (user) {
-        console.log("成功");
         return <Route exact={exact} path={path} component={component} />;
     } else {
-        console.log(user);
         return <Redirect to="/login" />;
     }
 };
